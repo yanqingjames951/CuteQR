b14cad65d39874b4ed83c72e86b3cce22a2e0f45
junyi <shuaijames666@gmail.com>
2024-09-29 23:56:51 +0800

    Initial Commit

---
+++ CuteQR/CuteQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuteQRApp.swift
+//  CuteQR
+//
+//  Created by WilliamJunYi on 2024/9/29.
+//
+
+import SwiftUI
+
+@main
+struct CuteQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CuteQRTests/CuteQRTests.swift
@@ -0,0 +1,17 @@
+//
+//  CuteQRTests.swift
+//  CuteQRTests
+//
+//  Created by WilliamJunYi on 2024/9/29.
+//
+
+import Testing
+@testable import CuteQR
+
+struct CuteQRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
